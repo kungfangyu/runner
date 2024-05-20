058c470110a15b8f7bb2d0d6bf8309a9b495e0ea
FangyuKung <fish19921026@gmail.com>
2024-05-21 00:49:41 +0100

    feat: create userClient and use jsonplaceholder data.

--- src/main/java/dev/fane/runnerz/Application.java
+++ src/main/java/dev/fane/runnerz/Application.java
@@ -1,9 +1,18 @@
 package dev.fane.runnerz;
 
+import dev.fane.runnerz.user.User;
+import dev.fane.runnerz.user.UserHttpClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.client.support.RestClientAdapter;
+import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+
+import java.util.List;
 
 @SpringBootApplication
 public class Application {
@@ -15,13 +24,23 @@ public class Application {
 
 	}
 
-//	@Bean
-//	CommandLineRunner runner (RunRepository runRepository) {
-//		return args -> {
-//			Run run = new Run(1, "First Run", LocalDateTime.now(),
-//					LocalDateTime.now().plus(1, ChronoUnit.HOURS), 5, Location.OUTDOOR);
-//			runRepository.create(run);
-//		};
-//	}
+	@Bean
+	UserHttpClient userHttpClient() {
+		RestClient restClient = RestClient.create("https://jsonplaceholder.typicode.com");
+		HttpServiceProxyFactory factory = HttpServiceProxyFactory
+				.builderFor(RestClientAdapter.create(restClient)).build();
+		return factory.createClient(UserHttpClient.class);
+	}
+
+	@Bean
+	CommandLineRunner runner (UserHttpClient client) {
+		return args -> {
+			List<User> users = client.findAll();
+			System.out.println(users);
+
+			User user = client.findById(1);
+			System.out.println(user);
+		};
+	}
 
 }
---
+++ src/main/java/dev/fane/runnerz/user/Company.java
@@ -0,0 +1,8 @@
+package dev.fane.runnerz.user;
+
+public record Company(
+        String name,
+        String catchPhrase,
+        String bs
+) {
+}
